21442557a8f268685d66a5aa44a3ddc857aa85c0
James Wilcox <jameswilcox.98@gmail.com>
2026-07-07 03:45:14 -0300

    actually finished arrays

--- arrays/sum.go
+++ arrays/sum.go
@@ -9,9 +9,21 @@ func Sum(nums []int) int {
 }
 
 func SumAll(slicesToSum... []int) []int {
-	ans := make([]int, len(slicesToSum))
-	for i, nums := range slicesToSum {
-		ans[i] = Sum(nums)
+	var ans []int
+	for _, nums := range slicesToSum {
+		ans = append(ans, Sum(nums))
+	}
+	return ans
+}
+
+func SumAllTails(slicesToSum... []int) []int {
+	var ans []int
+	for _, slice := range slicesToSum {
+		if len(slice) >= 1 {
+			ans = append(ans, Sum(slice[1:]))
+		} else {
+			ans = append(ans, 0)
+		}
 	}
 	return ans
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestSum(t *testing.T) {
 
-	t.Run("collection of any size", func(t *testing.T) {
+	t.Run("slice of any size", func(t *testing.T) {
 
 		numbers := []int{1, 2, 3}
 		
@@ -21,7 +21,7 @@ func TestSum(t *testing.T) {
 }
 
 func TestSumAll(t * testing.T) {
-	t.Run("2 collections", func(t *testing.T) {
+	t.Run("2 slices", func(t *testing.T) {
 
 		got := SumAll([]int{1, 2}, []int{9})
 		want := []int{3, 9}
@@ -29,5 +29,42 @@ func TestSumAll(t * testing.T) {
 		if !slices.Equal(got, want) {
 			t.Errorf("got %d want %d", got, want)
 		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func (t *testing.T, got, want []int) {
+		t.Helper()
+		if !slices.Equal(got, want) {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	}
+
+	t.Run("2 slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{3, 4, 5})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("A slice with only one element", func(t *testing.T) {
+		got := SumAllTails([]int{1})
+		want := []int{0}
+
+		checkSums(t, got, want)
+	})
+	
+	t.Run("A slice with no elements", func(t *testing.T) {
+		got := SumAllTails([]int{})
+		want := []int{0}
+		if !slices.Equal(got, want) {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("Two slices with no elements", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{})
+		want := []int{0, 0}
+		checkSums(t, got, want)
 	})
 }
